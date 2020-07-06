a15d53421dd4f6269b75848a9b5b0adbcb4ba13d
trajan <trajan.bronner@gmail.com>
2020-07-06 18:36:41 +0200

    feat(routing): add routing to login and got view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {GotViewComponent} from './got-view/got-view.component';
+import {GotViewModule} from './got-view/got-view.module';
+import {LoginComponent} from './login/login.component';
+import {LoginModule} from './login/login.module';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'got-view',
+    component: GotViewComponent,
+  },
+  {
+    path: '**',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [GotViewModule, LoginModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
