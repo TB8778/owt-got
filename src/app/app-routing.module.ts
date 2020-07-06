e4bc8549aff7751819adb68f65840923d33ff332
trajan <trajan.bronner@gmail.com>
2020-07-06 19:15:30 +0200

    feat(routing): add enum for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ import {GotViewModule} from './got-view/got-view.module';
 import {LoginComponent} from './login/login.component';
 import {LoginModule} from './login/login.module';
 
+export enum allRoutes {
+  login = 'login',
+  gotView = 'got-view'
+}
 
 const routes: Routes = [
   {
