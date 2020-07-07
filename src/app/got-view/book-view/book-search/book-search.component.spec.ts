df6461d5ffb25e77b2cff9c4903303dec6170de6
trajan <trajan.bronner@gmail.com>
2020-07-07 17:54:40 +0200

    feat(book-search): add component

---
+++ src/app/got-view/book-view/book-search/book-search.component.spec.ts
@@ -0,0 +1,26 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {BookViewModule} from '../book-view.module';
+
+import {BookSearchComponent} from './book-search.component';
+
+describe('BookSearchComponent', () => {
+  let component: BookSearchComponent;
+  let fixture: ComponentFixture<BookSearchComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [BookViewModule],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/got-view/book-view/book-search/book-search.component.ts
@@ -0,0 +1,57 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-book-search',
+  templateUrl: './book-search.component.html',
+  styleUrls: ['./book-search.component.scss'],
+})
+export class BookSearchComponent implements OnInit, OnDestroy {
+
+  @Input() forcedFilter: string;
+  @Output() newSearchValue = new EventEmitter<string>();
+
+  searchValue: string = '';
+  searchSubject: Subject = new Subject<string>();
+  searchDebounce$: Observable<string>;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    // Add debounce before firing @Output Event to smoothen search
+    this.searchDebounce$ = this.searchSubject.asObservable()
+      .pipe(
+        debounceTime(500),
+      )
+      .subscribe({
+        next: searchValue => {
+          this.newSearchValue.emit(searchValue);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.searchDebounce$?.unsubscribe();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes?.forcedFilter) {
+      console.log(changes.forcedFilter);
+      this.searchValue = '';
+    }
+  }
+
+  clearSearch($event: Event) {
+    // Prevent form submission
+    $event.preventDefault();
+    this.searchValue = '';
+    this.searchSubject.next(this.searchValue);
+  }
+
+  searchInputChanged() {
+    this.searchSubject.next(this.searchValue);
+  }
+
+}
