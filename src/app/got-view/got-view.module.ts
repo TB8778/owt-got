51ed57ed59cce92033c31c746587afa1e7cef91b
trajan <trajan.bronner@gmail.com>
2020-07-07 11:58:32 +0200

    feat(got-view): use subviews in tabs

--- src/app/got-view/got-view.module.ts
+++ src/app/got-view/got-view.module.ts
@@ -3,13 +3,22 @@ import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {BookViewModule} from './book-view/book-view.module';
+import {CharacterViewModule} from './character-view/character-view.module';
 import {GotViewComponent} from './got-view.component';
 import {HeaderComponent} from './header/header.component';
+import {HouseViewModule} from './house-view/house-view.module';
 
 @NgModule({
   declarations: [GotViewComponent, HeaderComponent],
   imports: [
-    CommonModule, MatTabsModule, MatToolbarModule, MatButtonModule,
+    BookViewModule,
+    CharacterViewModule,
+    CommonModule,
+    HouseViewModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
 })
 export class GotViewModule {
