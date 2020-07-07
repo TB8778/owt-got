1153e7ae1aeb2f69cde425d8dc0a4a6351e0f7bf
trajan <trajan.bronner@gmail.com>
2020-07-07 11:48:50 +0200

    feat(got-view): add header to main view and tabs for resources

--- src/app/got-view/got-view.module.ts
+++ src/app/got-view/got-view.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GotViewComponent } from './got-view.component';
-import { HeaderComponent } from './header/header.component';
-
-
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {GotViewComponent} from './got-view.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [GotViewComponent, HeaderComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule, MatTabsModule, MatToolbarModule, MatButtonModule,
+  ],
 })
-export class GotViewModule { }
+export class GotViewModule {
+}
